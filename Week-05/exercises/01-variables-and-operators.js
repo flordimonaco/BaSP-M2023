feacf6558fa425ad9687cb3b69170da21bd1d2c1
flordimonaco <flordimonaco@gmail.com>
2023-04-14 19:49:48 -0300

    BaSP-W05-2023:html-creation/first-block-solved

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,15 @@
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var myNumber = 7;
+var myNumber2 = 11;
+var suma = (myNumber + myNumber2)
+console.log(suma);
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var myWord = 'Rosario';
+var myWord2 = 'Central';
+var words = (myWord + myWord2);
+console.log(words);
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+var word = 'red';
+var word2 = 'wine';
+var result = (word.length + word2.length);
+console.log(result);
