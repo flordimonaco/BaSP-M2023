a57a86122bad236c81285ce30b13191b74147770
flordimonaco <flordimonaco@gmail.com>
2023-04-14 21:30:52 -0300

    BaSP-W05-2023:fourth-block-solved

---
+++ Week-05/exercises/04-if-else.js
@@ -0,0 +1,38 @@
+/* a. Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), 
+si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than or equal to 0,5” y sino un alerta con el mensaje “Lower than 0,5”.*/
+var randomNumber = Math.random();
+if (randomNumber >= 0.5) {
+  alert("Greater than or equal to 0.5");
+} else {
+  alert("Lower than 0.5");
+}
+/* b. Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Bebe” si la edad es menor a 2 años;
+“Niño” si la edad es entre 2 y 12 años;
+“Adolescente” entre 13 y 19 años;
+“Joven” entre 20 y 30 años;
+“Adulto” entre 31 y 60 años;
+“Adulto mayor” entre 61 y 75 años;
+“Anciano” si es mayor a 75 años.*/
+var age = '17'
+if (age < 2) {
+    console.log('bebé');
+}
+else if (age >= 2 && age <= 12) {
+    console.log('niño');
+}
+else if (age >= 13 && age <= 19) {
+    console.log('adolescente');
+}
+else if (age >= 20 && age <= 30) {
+    console.log('joven');
+}
+else if (age >= 31 && age <= 60) {
+    console.log('adulto');
+}
+else if (age >= 61 && age <= 75) {
+    console.log('adulto mayor');
+}
+else {
+    console.log('anciano');
+}
